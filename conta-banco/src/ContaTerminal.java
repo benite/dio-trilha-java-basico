d2ae78901565c1f928ea942df5d7b11ccf5afe13
Benite Ribeiro <beniterm@gmail.com>
2024-08-13 17:33:41 -0300

    desafio finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Consumir a nova linha deixada pelo nextInt
+
+        System.out.println("Digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco");
+        System.out.println("Sua agência é " + agencia + ", conta " + numero);
+        System.out.println("Seu saldo " + saldo + " já está disponível");
     }
 }
